4d24a9a2fccdb85603278b1e1407e988c1b27ee0
Camila Cano <camilacanog@gmail.com>
2013-11-02 19:11:51 -0400

    clean code

--- scripts/todo.js
+++ scripts/todo.js
@@ -3,26 +3,25 @@ function sanitize(str) {
 	var new_array = _.select(str_array, function(letter) {
 		return !letter.match(/['"]/);
 	} );
+
 	return new_array.join("");
-}
+};
 
-
-var TodoItem = function(text, due_date, color) {
+var TodoItem = function(text) {
 	var date = new Date();
 	this.text = text;
 	this.created = date.toLocaleTimeString();
 	this.completed = '';
-	this.due_date = due_date || new Date();
-	this.color = "green";
+	this.color = '';
 };
 
 var TodoApp = function() {
 	this.finished_tasks = [];
 	this.unfinished_tasks = [];
 
-	this.createTask = function(text, due_date, color) {
+	this.createTask = function(text, due_date) {
 		text = sanitize(text);
-		var task = new TodoItem(text, due_date, color);
+		var task = new TodoItem(text, due_date);
 		if (_.findWhere(this.unfinished_tasks, {text: text})) {
 			// do nothing!
 		} else {
@@ -34,7 +33,7 @@ var TodoApp = function() {
 
 	this.completeTask = function(el, text) {
 		var completed = new Date();
-		// var text = el.dataset.tasktext;
+		
 		el.remove();
 
 		var task = _.findWhere(this.unfinished_tasks, {text: text});
@@ -57,7 +56,6 @@ var TodoApp = function() {
 		el.getElementsByClassName('meta-data')[0].innerHTML = " completed at " + completed.toLocaleTimeString();
 		el.dataset.completed = 'true';
 		completed_items.appendChild(el);
-		console.log(el);
 
 	};
 
@@ -84,10 +82,12 @@ var TodoApp = function() {
 		var el = document.createElement('li');
 		el.className = "items";
 		var html = task.text;
-		
-		
+		var now = new Date();
+
+		setInterval()
+
 		if (!complete) {
-			html += "<span class='meta-data'> created at " + task.created + " </span>";
+			html += "    " + "<span class='meta-data'> created at " + task.created + " </span>";
 			var complete_btn = "<button class='complete' onclick=\"todo_app.completeTask(this.parentElement, '" + task.text + "')\"  >Complete</button>";
 			html += complete_btn;
 		} else {
@@ -96,6 +96,7 @@ var TodoApp = function() {
 
 		html += "<button class='delete' onclick=\"todo_app.deleteTask(this.parentElement, '" + task.text + "')\" >Delete</button>";
 		el.innerHTML = html;
+
 		if (complete) {
 			el.dataset.completed = 'true'
 			completed_items.appendChild(el);
@@ -109,7 +110,7 @@ var TodoApp = function() {
 
 var todo_app = new TodoApp();
 
-var add_item, new_task_field, todo_items, completed_items, stored_unfinished_tasks, stored_finished_tasks, due_date, day, year, month;
+var add_item, new_task_field, todo_items, completed_items, stored_unfinished_tasks, stored_finished_tasks, day, year, month;
 
 window.onload = function() {
 	add_item = document.getElementById('add-item');
@@ -120,9 +121,6 @@ window.onload = function() {
 	day = document.getElementById('day'); 
 	year = document.getElementById('year');
 	hour = document.getElementById('time');
-	
-	now = new Date();
-	console.log(due_date);
 
 	stored_unfinished_tasks = JSON.parse(localStorage.getItem('unfinished_tasks'));
 	stored_finished_tasks = JSON.parse(localStorage.getItem('finished_tasks'));
@@ -130,16 +128,8 @@ window.onload = function() {
 	todo_app.unfinished_tasks = stored_unfinished_tasks || [];
 	todo_app.finished_tasks = stored_finished_tasks || [];
 
-
 	_.each(stored_unfinished_tasks, function(task) {
 		// added the feature that changes the color in the object then i have to do that change the css 
-		if((task.due_date.getDate < (now.getDate + 2)) && (task.due_date.getDate > (now.getDate + 1))) {
-			task.color = "yellow"
-		}else if((task.due_date.getDate < (now.getDate + 1)) && (task.due_date.getDate > (now.getDate))){
-			task.color = "orange"
-		}else if(task.due_date.getDate < (now.getDate)){
-			task.color = "red"
-		}
 		todo_app.appendTask(task);
 	});
 
@@ -147,21 +137,17 @@ window.onload = function() {
 		todo_app.appendTask(task, true);
 	});
 
-
 	add_item.addEventListener('click', function(e) {
 		var text = new_task_field.value;
 		new_task_field.value = ""
-		var due_date = due_date
-		console.log(due_date);
+		var now = new Date();
+
 		todo_app.createTask(text, due_date);
 	});
 
 	new_task_field.addEventListener('keyup', function(e) {
-		// due_date is still not working
-	due_date = new Date(parseInt(year.value), (parseInt(month.value) - 1), parseInt(day.value), parseInt(hour.value));
 		if (e.keyCode == 13) {
-			var text = this.value;
-			var due_date = due_date
+			var text = this.value;	
 			this.value = ""
 			todo_app.createTask(text, due_date);
 		}
